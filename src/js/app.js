86c9a3e62b7a53939e3310abef333cedcf70d88a
salmanlidev <isimsizoyuncu550@gmail.com>
2023-02-10 23:43:00 +0400

    V1.2 added hero section

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 const hamburgerBtn = document.querySelector(".menu_hamburger")
 const menu = document.querySelector(".menu")
 
-
 hamburgerBtn.addEventListener("click", (e) => {
     hamburgerBtn.classList.toggle("activeHamburger")
     if (hamburgerBtn.classList.contains("activeHamburger")) {
@@ -25,7 +24,80 @@ hamburgerBtn.addEventListener("click", (e) => {
         <line x1="3" y1="6" x2="21" y2="6"></line>
         <line x1="3" y1="18" x2="21" y2="18"></line>
     </svg>`;
-    menu.style.transform = "scale(0)"
+        menu.style.transform = "scale(0)"
         menu.style.opacity = "0"
     }
 })
+
+// List of sentences
+var _CONTENT = [ 
+	"Frontend Devoloper", 
+	"Web Devoloper" ,
+    "UI Designer"
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#hero_about_type");
+
+// Cursor element 
+// var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+		// Hide the cursor
+		// _CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			// _CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 200);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 200);
+
+
