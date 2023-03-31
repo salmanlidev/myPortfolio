ad44d2bb368c7d7eeac9b6f1e3de9dccb46c6feb
salmanlidev <isimsizoyuncu550@gmail.com>
2023-03-31 22:26:37 +0400

    V2.31

---
+++ src/js/matrix.js
@@ -0,0 +1,35 @@
+let canvas = document.querySelector("canvas")
+let ctx = canvas.getContext("2d")
+let width = canvas.width = window.innerWidth
+let height = canvas.height = window.innerHeight
+
+let str = "sam ir 20 03 11 07"
+let matrix = str.split("")
+let font = 24 
+let col = width / font
+let arr = []
+
+for(let i = 0 ; i < col ; i++){
+    arr[i] = 1
+}
+
+const draw = () => {
+    ctx.fillStyle = "rgba( 0 , 0 , 0 , 0.05)"
+    ctx.fillRect(0 ,0  ,width , height)
+    ctx.fillStyle = "#4dfd21"
+    ctx.font = `${font}px system-ui`
+
+    for(let i = 0 ; i < arr.length ; i++){
+        let txt = matrix[Math.floor(Math.random() * matrix.length)]
+        ctx.fillText(txt , i * font , arr[i] * font)
+        
+        if(arr[i] * font > height && Math.random() > 0.975){
+            arr[i] = 0
+        }
+        arr[i]++
+    }
+}
+
+setInterval(draw , 20)
+
+window.addEventListener("resize" , () => location.reload())
